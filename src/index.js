3320f88956b18866ec0d7fd6f23b2768aba0da93
fauzyahmad <ahmadfauzycg@gmail.com>
2021-08-15 22:16:26 +0800

    adding data layer context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {DataLayer} from './context/DataLayer';
+import reducer from './context/reducer';
+import {initialState} from './context/reducer';
 import App from "./App";
 // import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
