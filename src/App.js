c388e94cb18d4a5957934fb5962a7a44e729cdbe
fauzyahmad <ahmadfauzycg@gmail.com>
2021-08-15 22:17:27 +0800

    app js edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useDataLayerValue } from './context/DataLayer';
+import InfoTaxi from './info_taxi';
+import MapDeckTaxi from './mapdeck_taxi';
+import { getTaxiAvailability } from './services';
 
 export default function App() {
+  const [loading, setLoading] = useState(false)
+  const [{dataTaxi}, dispatch] = useDataLayerValue()
+
+  const getDataTaxi = async() => {
+    setLoading(true)
+    let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    let localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1)
+    let responseData = await getTaxiAvailability(localISOTime);
+    dispatch({type: 'SET_DATA_TAXI', payload: responseData.features[0].geometry.coordinates })
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    // return () => {
+    //   effect
+    // };
+    getDataTaxi()
+  }, [])
   return (
     <>
-        <p>Helloworld</p>
+      { loading
+        ? <p></p>
+        : <div>
+            <MapDeckTaxi data={dataTaxi} />
+            <InfoTaxi />
+          </div>
+      }
     </>
   );
 }
