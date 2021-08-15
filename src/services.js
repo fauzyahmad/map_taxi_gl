3413f59ca80b8d61227838cf6d6b8db3a5aed2c8
fauzyahmad <ahmadfauzycg@gmail.com>
2021-08-15 22:17:03 +0800

    adding services to get taxi available in SG

---
+++ src/services.js
@@ -0,0 +1,22 @@
+const GET_TAXI_AVAILABILITY = 'https://api.data.gov.sg/v1/transport/taxi-availability?date_time=';
+import regeneratorRuntime from "regenerator-runtime";
+
+
+export const getTaxiAvailability = async(dateTime) => {
+    try {
+        let formatDateISO = dateTime.split('.')[0]+"Z"
+        let response = await fetch(GET_TAXI_AVAILABILITY+formatDateISO, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.geo+json"
+            }
+        });
+        let responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        //error handling
+        //console.log(error);
+        return error;
+    }
+}
