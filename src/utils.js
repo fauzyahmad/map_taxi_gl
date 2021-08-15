3fa6e200040bea60e1e4dbcee2640a566ee3f608
fauzyahmad <ahmadfauzycg@gmail.com>
2021-08-15 22:17:19 +0800

    initial state mapbox added

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWhtYWRmYXV6eSIsImEiOiJja3M4dmY2Y3gxcW14MnVwaDl1Z3UwdHY1In0.Iyf59C0ac2Afh8K_Vo6DBw';
+
+export let INITIAL_VIEW_STATE = {
+    longitude: 103.810591,
+    latitude: 1.35219,
+    zoom: 11,
+    pitch: 0,
+    bearing: 0
+};
